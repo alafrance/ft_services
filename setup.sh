9d07260dd2efeda13fe06c2198aa1578994b1dde
Alex <alexislafrances-laf@outlook.fr>
2021-03-22 11:43:40 +0100

    Revert "phpmyadmin finished"
    This reverts commit bae98f563e0a8a1ea25ceaeb2e6576c738e144f0.

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ function setup_metallb()
     kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
     kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
     kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-    kubectl apply -f ./srcs/metallb/.
+    kubectl apply -f ./srcs/metallb/metallb.yaml
+    kubectl apply -f ./srcs/metallb/database_pvc.yaml
 }
 
 function setup()
