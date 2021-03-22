c51753b01ef48d757d9743a8443cf627f8f50054
Alex <alexislafrances-laf@outlook.fr>
2021-03-22 11:59:29 +0100

    phpmyadmin no more error

--- srcs/mysql/srcs/script.sh
+++ srcs/mysql/srcs/script.sh
@@ -8,8 +8,9 @@ rc-service mariadb start
 
 
 echo "CREATE DATABASE wordpress;" | mysql -u root
+echo "CREATE DATABASE phpmyadmin;" | mysql -u root
 echo "CREATE USER 'root'@'%' IDENTIFIED BY 'root';" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
 rc-service mariadb restart
